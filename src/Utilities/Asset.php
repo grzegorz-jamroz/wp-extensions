b317f6b8c1dcb26dd9fe96b33254a6cc8918a17b
Grzegorz <grzegorz.jamroz@esatto.se>
2019-12-19 09:59:41 +0100

    Added Asset class

---
+++ src/Utilities/Asset.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace Grzechu\Utilities;
+
+class Asset
+{
+    private $entryName;
+    private $entrypointName;
+    private $extension;
+
+    public function __construct(
+        string $entryName,
+        string $entrypointName,
+        string $extension
+    ) {
+        $this->entryName = $entryName;
+        $this->entrypointName = $entrypointName;
+        $this->extension = $extension;
+    }
+
+    public function getSrc(): string
+    {
+        $this->entrypointName = sprintf('%s/%s', get_template_directory(), $this->entrypointName);
+        $path = sprintf('%s/%s*.%s', $this->entrypointName, $this->entryName, $this->extension);
+        $path = glob($path)[0] ?? sprintf('%s/%s.%s', $this->entrypointName, $this->entryName, $this->extension);
+        $path = str_replace(get_template_directory(), '', $path);
+
+        return sprintf('%s/%s', get_template_directory_uri(), $path);
+    }
+}
