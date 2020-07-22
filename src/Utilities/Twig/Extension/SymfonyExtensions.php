b8ad174e9a35d5dc6b23bc2fd02c86c938cb83fc
Grzechu <grzegorz.jamrooz@gmail.com>
2020-07-22 09:37:21 +0200

    added possibility to set root theme directory for asset function

--- src/Utilities/Twig/Extension/SymfonyExtensions.php
+++ src/Utilities/Twig/Extension/SymfonyExtensions.php
@@ -13,6 +13,11 @@ class SymfonyExtensions
 
     public function getAssetUrl(string $path, string $entrypointName = 'dist'): string
     {
+        if ($entrypointName === ''
+            || $entrypointName === '/') {
+            return sprintf('%s/%s', get_template_directory_uri(), $path);
+        }
+
         return sprintf('%s/%s/%s', get_template_directory_uri(), $entrypointName, $path);
     }
 }
