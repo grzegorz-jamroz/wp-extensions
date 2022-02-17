42be98eb97c344826f8abbc90befede47f986f50
murzyn <grzegorz.jamrooz@gmail.com>
2022-02-17 18:03:44 +0100

    added isPublic property to AjaxFormAction

--- src/FormAction/AjaxFormAction.php
+++ src/FormAction/AjaxFormAction.php
@@ -5,11 +5,22 @@ namespace Grzechu\FormAction;
 
 abstract class AjaxFormAction extends FormAction
 {
+    protected bool $isPublic;
+
+    public function __construct(string $action, bool $isPublic = false)
+    {
+        $this->isPublic = $isPublic;
+        parent::__construct($action);
+    }
+
     public function init()
     {
         add_action('wp_enqueue_scripts', [$this, 'enqueueScripts']);
         add_action(sprintf('wp_ajax_%s', $this->action), [$this, 'submit']);
-        add_action(sprintf('wp_ajax_nopriv_%s', $this->action), [$this, 'submit']);
+
+        if ($this->isPublic) {
+            add_action(sprintf('wp_ajax_nopriv_%s', $this->action), [$this, 'submit']);
+        }
     }
 
     abstract public function enqueueScripts();
